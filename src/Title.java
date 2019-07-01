3e2af0d5e59228b0d5bf32780fe804f2a4dc92ab
Andrzej Szypulski <szypul86@gmail.com>
2019-07-01 12:01:13 +0200

    Title fulfills specification

--- src/Title.java
+++ src/Title.java
@@ -8,7 +8,7 @@ public class Title {
         Scanner scanner = new Scanner(file);
         //COUNTER OF LINES/TITLES IN FILE
         int countTitles = 0;
-        String title = " ";
+        String title = "";
         //unless the file has no more lines:
         while(scanner.hasNextLine()) {
             //scan a line and store it in line variable
@@ -31,20 +31,52 @@ public class Title {
 
 
         int titleLength = (title.length());
-        char[] encrypted = new char[titleLength];
+        String encrypted = "";
         // encrypting the title in a new variable that will be a place to store and change the answers
         for (int i=0; i<titleLength; i++) {
             if (title.charAt(i) != ' ')
-                encrypted[i] = '_';
+                encrypted = encrypted + '_';
             else
-                encrypted[i] = title.charAt(i);
+                encrypted = encrypted + title.charAt(i);
         }
         //internal check:
         System.out.println(encrypted);
-        
 
+        int scoreToloose =10;
+        int wrongLetter = 0;
+        String wrongLetters = "";
+        Scanner guessScanner = new Scanner(System.in);
 
+        int indexOf = 0;
 
+        while (!encrypted.equals(title)) {
+            System.out.println("You are guessing: " + encrypted);
+            System.out.println("You have guessed " + wrongLetter + " wrong letters:" + wrongLetters);
+            System.out.println("Guess a letter:");
+            char letter = guessScanner.next().charAt(0);
 
+            //System.out.println("letter:" + letter);
+            if (title.indexOf(letter) < 0) {
+                wrongLetter++;
+                wrongLetters = wrongLetters + ' ' + letter;
+                if (wrongLetter >= scoreToloose)
+                    break;
+            }
+            else {
+
+                while (title.indexOf(letter, indexOf) >= 0) {
+                    indexOf = title.indexOf(letter, indexOf);
+                    encrypted = encrypted.substring(0,indexOf) + letter + encrypted.substring(indexOf +1);
+                    indexOf ++;
+                }
+                indexOf = 0;
+            }
+        }
+        if (wrongLetter < scoreToloose)
+            System.out.println("Congratulations, you win!");
+        else
+            System.out.println("You loose, Looser");
+
+        System.out.println("The movie title is:" + title);
     }
 }
